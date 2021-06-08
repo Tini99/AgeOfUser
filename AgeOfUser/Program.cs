69e121cfded0a7e4791da5bfc9ff8e35204f5b0e
Tini Gisa <1814759@nzse.ac.nz>
2021-06-08 23:15:01 +1200

    Add project files.

---
+++ AgeOfUser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AgeOfUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+
+            Console.WriteLine("please enter number1");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            if(number1 > 18)
+            {
+                Console.WriteLine("you are and adult now");
+            }
+            if(number1 < 18)
+            {
+                Console.WriteLine("you are minor");
+            }
+        }
+    }
+}
